3394978e68a627c94eee351106b055646bca2639
Bhanu Prakash Dixit <74755575+hrvrbhanu01@users.noreply.github.com>
2023-11-28 10:02:40 +0530

    ,

--- secrets/secretRoutes.js
+++ secrets/secretRoutes.js
@@ -1,6 +1,9 @@
 const express = require("express");
+
 const router = express.Router();
+
 const {retrieveSecrets,storeSecrets} = require("./dbsecrets")
+
 router.route("/retrieveSecrets/:secretName").get(retrieveSecrets);
 router.post("/storeSecrets", storeSecrets)
 
